73b59e11a2e571c59dc3a5fb64631209eaaf8d43
Manpreet Singh <62533486+singhmnprt01@users.noreply.github.com>
2020-04-15 17:57:01 +0530

    Update setup.py

--- customdnn/setup.py
+++ customdnn/setup.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 15 16:04:59 2020
 
-@author: manpreetsi
+"""
+@author: singhmnprt01
 """
 
 from setuptools import setup
@@ -12,19 +9,18 @@ setup(name="customdnn",
       version = "0.1",
       description ="A custom deep neural network package that gives the liberty to design a customized neural network (using NumPy only)",
       url="https://github.com/singhmnprt01/Custom-Deep-Neural-Network",
-      author="Manpreet Singh",
+      author="singhmnprt01",
       packages = ['customdnn'],
       install_requires =['pandas','numpy','matplotlib','sklearn','datetime','sys'],
       long_description = "README"
-      classifiers=[  # Optional
+      classifiers=[  
          'Development Status :: 3 - Alpha',
 
-   
         'Intended Audience :: Deep Learning Learners',
         'Topic :: Software Development :: Build Tools',
 
-        # Pick your license as you wish
         'License :: GNU', 
+            
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
