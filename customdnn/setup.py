25c34e4df68c9c9da34a77108b5e764a85cf318e
Manpreet <62533486+singhmnprt01@users.noreply.github.com>
2020-05-01 17:20:45 +0530

    update setup version

--- customdnn/setup.py
+++ customdnn/setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup
 
 setup(name="customdnn",
-      version="1.0.4",
+      version="1.0.5",
       description="A custom deep neural network package that gives the liberty to design a customized neural network (using NumPy only)",
       url="https://github.com/singhmnprt01/Custom-Deep-Neural-Network",
       author="singhmnprt01@gmail.com",
